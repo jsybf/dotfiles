161afc70993bd3ab52acd779e907d8a26e581eb1
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2024-11-28 23:47:29 +0900

    set Oil show hidden file except .. and .git

--- .config/nvim/lua/oil-conf.lua
+++ .config/nvim/lua/oil-conf.lua
@@ -2,6 +2,21 @@ return {
   'stevearc/oil.nvim',
   opts = {},
   config = function()
-    require('oil').setup()
+    require('oil').setup {
+      view_options = {
+
+        show_hidden = true,
+
+        -- This function defines what is considered a "hidden" file
+        is_hidden_file = function(name, bufnr)
+          return vim.startswith(name, '.')
+        end,
+
+        -- This function defines what will never be shown, even when `show_hidden` is set
+        is_always_hidden = function(name, bufnr)
+          return name == '..' or name == '.git'
+        end,
+      }, -- view_options end
+    }
   end,
 }
