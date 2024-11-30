735be87741a577df72da9754129515555113beb3
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2024-11-30 20:24:50 +0900

    add yaml lsp and aws cloudformation support

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -504,7 +504,46 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
-
+        yamlls = {
+          settings = {
+            yaml = {
+              -- for cloudformation tags
+              customTags = {
+                '!Cidr',
+                '!Cidr sequence',
+                '!And',
+                '!And sequence',
+                '!If',
+                '!If sequence',
+                '!Not',
+                '!Not sequence',
+                '!Equals',
+                '!Equals sequence',
+                '!Or',
+                '!Or sequence',
+                '!FindInMap',
+                '!FindInMap sequence',
+                '!Base64',
+                '!Join',
+                '!Join sequence',
+                '!Ref',
+                '!Sub',
+                '!Sub sequence',
+                '!GetAtt',
+                '!GetAZs',
+                '!ImportValue',
+                '!ImportValue sequence',
+                '!Select',
+                '!Select sequence',
+                '!Split',
+                '!Split sequence',
+              },
+              schemas = {
+                ['https://raw.githubusercontent.com/awslabs/goformation/master/schema/cloudformation.schema.json'] = '*-cfn.{yml,yaml}',
+              },
+            },
+          },
+        },
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
