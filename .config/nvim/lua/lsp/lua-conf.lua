d6a6bae3120dbd112227bb0c296cc745d9470815
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2025-01-01 23:27:38 +0900

    disable lua unused diagnostics

--- .config/nvim/lua/lsp/lua-conf.lua
+++ .config/nvim/lua/lsp/lua-conf.lua
@@ -6,6 +6,20 @@ return {
       },
       -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
       -- diagnostics = { disable = { 'missing-fields' } },
+      diagnostics = {
+        disable = {
+          'unused',
+          'unused-function',
+          'code-after-break',
+          'empty-block',
+          'redundant-return',
+          'trailing-space',
+          'unreachable-code',
+          'unused-label',
+          'unused-local',
+          'unused-varag',
+        },
+      },
     },
   },
   -- cmd = {...},
