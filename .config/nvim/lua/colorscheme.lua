bdad1b34c1be4d8d17486a85bf5e68f92a594a73
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2025-01-13 14:58:50 +0900

    change colorscheme of neovim to Kanagawa

--- .config/nvim/lua/colorscheme.lua
+++ .config/nvim/lua/colorscheme.lua
@@ -1,9 +1,29 @@
 return {
-  'bluz71/vim-moonfly-colors',
-  name = 'moonfly',
+  'rebelot/kanagawa.nvim',
   lazy = false,
   priority = 1000,
+  opts = {
+    colors = {
+      theme = {
+        all = {
+          ui = {
+            bg_gutter = 'none',
+          },
+        },
+      },
+    },
+  },
   init = function()
-    vim.cmd [[colorscheme moonfly]]
+    vim.cmd.colorscheme 'kanagawa-wave'
   end,
 }
+
+-- return {
+--   'bluz71/vim-moonfly-colors',
+--   name = 'moonfly',
+--   lazy = false,
+--   priority = 1000,
+--   init = function()
+--     vim.cmd [[colorscheme moonfly]]
+--   end,
+-- }
