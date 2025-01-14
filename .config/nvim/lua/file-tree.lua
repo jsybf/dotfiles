269a6eeba4ffa1aa030448602dd485c6d77e44ab
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2025-01-14 23:39:18 +0900

    i have no idea

--- .config/nvim/lua/file-tree.lua
+++ .config/nvim/lua/file-tree.lua
@@ -11,10 +11,10 @@ return {
     vim.g.loaded_netrwPlugin = 1
 
     require('nvim-tree').setup {
-      update_focused_file = {
-        enable = true,
-        update_cwd = true,
-      },
+      -- update_focused_file = {
+      --   enable = true,
+      --   update_cwd = true,
+      -- },
       renderer = {
         icons = {
           show = {
