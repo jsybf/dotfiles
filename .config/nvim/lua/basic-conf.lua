da135945e9605b2276fefc3147758e806dff1626
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2025-01-15 15:08:35 +0900

    더 스무스한 마우스 스크롤링과 scrolloff 설정

--- .config/nvim/lua/basic-conf.lua
+++ .config/nvim/lua/basic-conf.lua
@@ -1,16 +1,16 @@
 -- Minimal number of screen lines to keep above and below the cursor.
 -- set cursor always middle
-vim.opt.scrolloff = 5
+vim.opt.scrolloff = 10
 
-vim.cmd 'set mousescroll=ver:3'
+vim.cmd 'set mousescroll=ver:1'
 
 -- remove status bar
 -- vim.o.cmdheight = 0
 vim.opt.laststatus = 0
-vim.api.nvim_set_hl(0 , 'Statusline', {link = 'Normal'})
-vim.api.nvim_set_hl(0 , 'StatuslineNC', {link = 'Normal'})
+vim.api.nvim_set_hl(0, 'Statusline', { link = 'Normal' })
+vim.api.nvim_set_hl(0, 'StatuslineNC', { link = 'Normal' })
 -- local str = string.repeat('-', vim.api.nvim_win_get_width(0))
-vim.opt.statusline = ""
+vim.opt.statusline = ''
 
 vim.api.nvim_create_autocmd('ColorScheme', {
   pattern = '*',
