5e69d952a3832aec6980ccfcab243af079ab5358
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2025-01-18 04:53:19 +0900

    tab누를 때 첫 추천결과를 건너뛰고 2번째 부터 선택되는거 해결

--- .config/nvim/lua/nvim-cmp-conf.lua
+++ .config/nvim/lua/nvim-cmp-conf.lua
@@ -1,70 +1,66 @@
-return { -- Autocompletion
+local function tab_select_next_mapping(fallback)
+  local cmp = require 'cmp'
+  if cmp.visible() then
+    -- tab누를 때 첫 추천결과를 건너뛰고 2번째 부터 선택되는거 해결
+    if cmp.get_selected_index() == 1 and not cmp.get_active_entry() then
+      cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
+      cmp.select_prev_item { behavior = cmp.SelectBehavior.Insert }
+    else
+      cmp.select_next_item { behavior = cmp.SelectBehavior.Insert }
+    end
+  else
+    fallback()
+  end
+end
+
+local function setup_cmp_cmdline()
+  local cmp = require 'cmp'
+  cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' },
+    },
+  })
+
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' },
+    }, {
+      {
+        name = 'cmdline',
+        option = {
+          ignore_cmds = { 'Man', '!' },
+        },
+      },
+    }),
+  })
+end
+
+return {
   'hrsh7th/nvim-cmp',
-  -- disable it because of cmp-cmdline should work in normal mode
-  -- event = 'InsertEnter',
   dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
     {
       'L3MON4D3/LuaSnip',
       build = (function()
-        -- Build Step is needed for regex support in snippets.
-        -- This step is not supported in many windows environments.
-        -- Remove the below condition to re-enable on windows.
         if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
           return
         end
         return 'make install_jsregexp'
       end)(),
-      dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
-        -- {
-        --   'rafamadriz/friendly-snippets',
-        --   config = function()
-        --     require('luasnip.loaders.from_vscode').lazy_load()
-        --   end,
-        -- },
-      },
+      dependencies = {},
     },
     'saadparwaiz1/cmp_luasnip',
-
-    -- Adds other completion capabilities.
-    --  nvim-cmp does not ship with all sources by default. They are split
-    --  into multiple repos for maintenance purposes.
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-cmdline',
   },
   config = function()
-    -- See `:help cmp`
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
     luasnip.config.setup {}
 
-    local function setup_cmp_cmdline()
-      cmp.setup.cmdline('/', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' },
-        },
-      })
-
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' },
-        }, {
-          {
-            name = 'cmdline',
-            option = {
-              ignore_cmds = { 'Man', '!' },
-            },
-          },
-        }),
-      })
-    end
     setup_cmp_cmdline()
 
     cmp.setup {
@@ -73,76 +69,13 @@ return { -- Autocompletion
           luasnip.lsp_expand(args.body)
         end,
       },
-      performance = {
-        max_view_entries = 10,
-      },
+      performance = { max_view_entries = 10 },
       completion = { completeopt = 'menu,menuone,noinsert' },
-
-      -- For an understanding of why these mappings were
-      -- chosen, you will need to read `:help ins-completion`
-      --
-      -- No, but seriously. Please read `:help ins-completion`, it is really good!
-      mapping = cmp.mapping.preset.insert {
-        -- intellij like mapping
-        -- ['<Tab>'] = cmp.mapping(function(fallback)
-        --   -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
-        --   if cmp.visible() then
-        --     local entry = cmp.get_selected_entry()
-        --     if not entry then
-        --       cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
-        --     end
-        --     cmp.confirm()
-        --   else
-        --     fallback()
-        --   end
-        -- end, { 'i', 's', 'c' }),
-        -- Select the [n]ext item
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        -- Select the [p]revious item
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-
-        -- Scroll the documentation window [b]ack / [f]orward
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-        -- Accept ([y]es) the completion.
-        --  This will auto-import if your LSP supports it.
-        --  This will expand snippets if the LSP sent a snippet.
-        ['<C-y>'] = cmp.mapping.confirm { select = true },
-
-        -- If you prefer more traditional completion keymaps,
-        -- you can uncomment the following lines
-        ['<CR>'] = cmp.mapping.confirm { select = true },
-        ['<Tab>'] = cmp.mapping.select_next_item(),
-        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-        -- Manually trigger a completion from nvim-cmp.
-        --  Generally you don't need this, because nvim-cmp will display
-        --  completions whenever it has completion options available.
+      mapping = {
+        ['<Tab>'] = cmp.mapping(tab_select_next_mapping, { 'i' }),
         ['<C-Space>'] = cmp.mapping.complete {},
-
-        -- Think of <c-l> as moving to the right of your snippet expansion.
-        --  So if you have a snippet that's like:
-        --  function $name($args)
-        --    $body
-        --  end
-        --
-        -- <c-l> will move you to the right of each of the expansion locations.
-        -- <c-h> is similar, except moving you backwards.
-        ['<C-l>'] = cmp.mapping(function()
-          if luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          end
-        end, { 'i', 's' }),
-        ['<C-h>'] = cmp.mapping(function()
-          if luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          end
-        end, { 'i', 's' }),
-
-        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
+
       sources = {
         {
           name = 'lazydev',
