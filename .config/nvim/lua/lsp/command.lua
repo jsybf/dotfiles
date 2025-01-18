fa49d36eb3b7f2d1ef70c276f78da59a362db056
park_jun_hyoung <gitpdeathwa@yonsei.ac.kr>
2025-01-18 22:30:22 +0900

    add lsp workspce symbol search command("Lssw")

---
+++ .config/nvim/lua/lsp/command.lua
@@ -0,0 +1,12 @@
+local logger = require 'logging'
+local function search_symbol(args)
+  local query = args['fargs'][1] or ''
+
+  local workspace_dirs = vim.lsp.buf.list_workspace_folders()
+  logger.info('searched workspace_dir: ' .. vim.inspect(workspace_dirs))
+  logger.info('searching_query: ' .. query)
+
+  vim.lsp.buf.workspace_symbol(query)
+end
+
+vim.api.nvim_create_user_command('Lssw', search_symbol, { nargs = '?', desc = 'lsp search symbols in workspace' })
